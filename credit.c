9e282c9ea8e338c61dc7a8ae2c0f133e9a01efa4
jakebbianca <jakebbianca@users.noreply.github.com>
2020-04-14 21:35:10 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- credit.c
+++ credit.c
@@ -15,18 +15,18 @@ int main(void)
         //Removes right-most digit from ccnum1, then assigns value of mod10 as dig0
         //This serves to make the first 'other' digit the right-most, then to claim as the remainder in dig0
         dig0 = (ccnum1 / 10) % 10;
-        //Multiply the digit by 2 per Luhn
+        //If the original product has only one digit, make prod that digit
         prod = dig0 * 2;
         //If the product has more than one digit, we must sum all of the digits together
         if (prod >= 10)
         {
             //digl is the left digit, so we must subtract the ones digit from the full product
-            digl0 = (prod - prod % 10);
+            digl0 = (prod / 10) % 10;
             //Product should only ever have 2 digits here, so the mod10 gets us the ones/right digit
             digr0 = prod % 10;
             //We use product for this sum so we can use it again after the if condition
             prod = digl0 + digr0;
-        }
+        }   
         //We sum the totals from the applicable options above, for either 1 or 2 digits
         sum0 = sum0 + prod;
         //We divide the copy number by 100 to remove the two digits handled in the previous loop
