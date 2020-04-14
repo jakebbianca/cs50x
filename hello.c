798b7d55a2417905a35b811dcf9c3cb1a120e944
jakebbianca <jakebbianca@users.noreply.github.com>
2020-04-14 01:58:41 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- hello.c
+++ hello.c
@@ -1,8 +1,13 @@
+//Conditions and relational operators
+
 #include <stdio.h>
 #include <cs50.h>
 
 int main(void)
 {
+    //Prompt user for answer
     string answer = get_string("What is your name?\n");
+
+    //Respond to user input for answer
     printf("hello, %s\n", answer);
 }
