2bc1d12a7d1ad560020369a932ed40c96034c363
jakebbianca <jakebbianca@users.noreply.github.com>
2020-04-22 03:39:35 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -4,6 +4,7 @@
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 //argc meaning "argument count", argv[] meaning "argument vector"
 //basically, count of arguments including the program ./ and the actual strings input on the command line from the user (hence the array of strings)
 int main(int argc, string argv[])
@@ -50,26 +51,28 @@ int main(int argc, string argv[])
             return 1;
         }
     }
-    //Ask user for plaintext input
+    //Ask user for plaintext to transform
     string pt = get_string("plaintext: ");
-    string ct = pt;
-    //Conversion of plaintext to cyphertext
-    for (int i = 0; pt[i] != '\0'; i++)
+    //Create base alphabet string and find conversions from key
+    string upperabc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string lowerabc = "abcdefghijklmnopqrstuvwxyz";
+    //Print "ciphertext " ahead of decoded printf's
+    printf("ciphertext: ");
+    for (int i = 0; (int) key[i] != '\0'; i++)
     {
-        int pt0 = pt[i];
-        int ptplus3 = (int) pt[i] + 3;
-        int ptminus23 = (int) pt[i] - 23;
-        //if a given letter's ASCII decimal value + 3 does not go past bound of z, regardless of case, modify the ct letter int value by + 3
-        if ((pt0 >= 65 && pt0 <= 90 && ptplus3 >= 65 && ptplus3 <= 90) || (pt0 >= 97 && pt0 <= 122 && ptplus3 >= 97 && ptplus3 <= 122))
+        if (islower(key[i]) != 0)
         {
-            ct[i] = (char) ptplus3;
+            printf("%c", lowerabc[i]);
         }
-        else if ((pt0 >= 65 && pt0 <= 90 && ptplus3 > 90) || (pt0 >= 97 && pt0 <= 122 && ptplus3 > 122))
+        else if (isupper(key[i]) != 0)
         {
-            ct[i] = (char) ptminus23;
+            printf("%c", upperabc[i]);
+        }
+        else
+        {
+            printf("Unknown error occurred while deciphering user plaintext.");
+            exit(0);
+            return 1;
         }
     }
-    //Print output, maintaining letter case if character is alphabetical
-    printf("cyphertext: %s", ct);
-    printf("\n");
 }
