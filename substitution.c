f9f467008d387bda3f4726b38b8b7dbaaf063505
jakebbianca <jakebbianca@users.noreply.github.com>
2020-04-23 00:50:33 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -15,14 +15,14 @@ int main(int argc, string argv[])
     if (argc != 2)
     {
         printf("Error: User must provide exactly one command line argument.\n");
-        exit(0);
+        exit(1);
         return 1;
     }
     //if the length of the command line argument string is NOT 26 characters, print error...
     if (strlen(key) != 26)
     {
         printf("Error: Input key length must be exactly 26 characters.\n");
-        exit(0);
+        exit(1);
         return 1;
     }
     //if agrv[1] does not exclusively alphabetical characters...
@@ -33,46 +33,45 @@ int main(int argc, string argv[])
         if ((int) letter0 < 65 || ((int) letter0 > 90 && (int) letter0 < 97) || (int) letter0 > 122)
         {
             printf("Error: Input key must contain only alphabetical characters.\n");
-            exit(0);
+            exit(1);
             return 1;
         }
     }
     //if argv[1] contains any duplicate alphabetical characters...
-    for (int i = 1; key[i] != '\0'; i++)
+    for (int i = 0; key[i] != '\0'; i++)
     {
-        char letter0 = key[0];
-        char letter1 = key[i];
-        int ascii0 = (int) letter0;
-        int ascii1 = (int) letter1;
-        if (ascii0 == ascii1 || ascii0 == ascii1 - 32 || ascii0 == ascii1 + 32)
+        int j = i + 1;
+        while (key[j] != '\0')
         {
-            printf("Error: Input key must not contain any duplicate letters.\n");
-            exit(0);
-            return 1;
+            if (key[i] == key[j])
+            {
+                printf("Error: Input key must not contain any duplicate letters.\n");
+                exit(1);
+                return 1;
+            }
+            j++;
         }
     }
-    //Ask user for plaintext to transform
     string pt = get_string("plaintext: ");
-    //Create base alphabet string and find conversions from key
-    string upperabc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    string lowerabc = "abcdefghijklmnopqrstuvwxyz";
-    //Print "ciphertext " ahead of decoded printf's
-    printf("ciphertext: ");
-    for (int i = 0; (int) key[i] != '\0'; i++)
+    int ptlen = strlen(pt);
+    printf("ciphertext ");
+    for (int i = 0; i < ptlen; i++)
     {
-        if (islower(key[i]) != 0)
+        if (islower(pt[i]) != 0)
         {
-            printf("%c", lowerabc[i]);
+            int map = pt[i] - 97;
+            printf("%c", tolower(key[map]));
         }
-        else if (isupper(key[i]) != 0)
+        else if (isupper(pt[i]) != 0)
         {
-            printf("%c", upperabc[i]);
+            int map = pt[i] - 65;
+            printf("%c", toupper(key[map]));
         }
         else
         {
-            printf("Unknown error occurred while deciphering user plaintext.");
-            exit(0);
-            return 1;
+            printf("%c", pt[i]);
         }
     }
+    printf("\n");
+    return 0;
 }
