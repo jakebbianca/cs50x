fbaf6477f6aa3721fa35a6086da29103e91a6f41
jakebbianca <jakebbianca@users.noreply.github.com>
2020-05-13 19:19:14 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -70,7 +70,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            if ((float)j < mirror || (float)j > mirror)
+            if (j < mirror || j > mirror)
             {
                 tmparr[i][j] = image[i][width - j - 1];
             }
