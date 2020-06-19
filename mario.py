ab673cafc136eb4b73d6c216aa3de692267f1954
jakebbianca <jakebbianca@users.noreply.github.com>
2020-06-19 20:49:23 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -4,19 +4,25 @@ def get_height():
     h = get_int("Height: ")
     return h
 
+
 def confirm_height():
     h = get_height()
 
     while h < 1 or h > 8:
         h = get_height()
 
+
     return h
 
 def main(h):
     n = 1
     while n <= h:
-        print(f" " * (h - n), "#" * n, "", f"#" * n)
+        if n == h:
+            print(f"#" * n, "", "#" * n)
+        else:
+            print(f" " * (h - n - 1), "#" * n, "", "#" * n)
         n += 1
+
 
 h = confirm_height()
 
