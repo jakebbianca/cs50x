da34ee65c1dfcbd176b737bcdf0247f927b0cbe0
jakebbianca <jakebbianca@users.noreply.github.com>
2020-08-05 00:08:39 +0000

    automated commit by submit50

---
+++ includeStyles.js
@@ -0,0 +1,2 @@
+document.write("<link href=\"https:\/\/stackpath.bootstrapcdn.com\/bootstrap\/4.1.3\/css\/bootstrap.min.css\" rel=\"stylesheet\">");
+document.write("<link href=\"styles.css\" rel=\"stylesheet\">");
---
+++ navBar.js
@@ -0,0 +1,7 @@
+document.write("<ul class=\"ulNav\">");
+document.write("    <li class=\"liNav\"><a href=\"index.html\" class=\"aNav\">Home<\/a><\/li>");
+document.write("    <li class=\"liNav\"><a href=\"music.html\" class=\"aNav\">Music<\/a><\/li>");
+document.write("    <li class=\"liNav\"><a href=\"question.html\" class=\"aNav\">Tough Question<\/a><\/li>");
+document.write("    <li class=\"liNav\"><a href=\"rules.html\" class=\"aNav\">Sports Rules<\/a><\/li>");
+document.write("    <li class=\"liNavAbout\"><a href=\"about.html\" class=\"aNav\">About<\/a><\/li>");
+document.write("<\/ul>");
