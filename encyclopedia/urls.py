89fefa9f40716641d2b46b2af113ed6c2689f43f
Jake Bianca <jakebbianca@gmail.com>
2020-11-19 14:51:49 -0500

    updated renders that displayed like redirects to true redirects

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>/", views.entry, name="entry"),
-    path("search", views.search, name="search")
+    path("no_entry/", views.no_entry, name="no_entry"),
+    path("search", views.search, name="search"),
+    path("new/", views.new, name="new")
 ]
