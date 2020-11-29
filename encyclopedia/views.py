f524ed9563c9ccab15300a47cc035d7ea2ed419c
Jake Bianca <jakebbianca@gmail.com>
2020-11-28 19:58:01 -0500

    added comments, completed implementation of entry creation page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -6,6 +6,8 @@ import markdown2
 
 from . import util
 
+
+# define custom django form classes
 class newSearchForm(forms.Form):
     q = forms.CharField(label="Search Encyclopedia")
 
@@ -15,85 +17,101 @@ class newEntryForm(forms.Form):
 
 
 def index(request):
-
+    
+    # render a page with a list of all wiki entries
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries(), "searchForm": newSearchForm()
     })
 
+
 def entry(request, title):
 
+    # if no entry exists for a given title, redirect to a 'not found' page
     if not util.get_entry(title):
         return HttpResponseRedirect(reverse("no_entry"))
     
+    # if entry does exist, return that entry's contents
     return render(request, "encyclopedia/entry.html", {
         "title": title, "content": markdown2.markdown(util.get_entry(title)), "searchForm": newSearchForm()
     })
 
+
 def no_entry(request):
+    # not found page for nonexistent entries
     return render(request, "encyclopedia/no_entry.html", {
         "searchForm": newSearchForm()
     })
 
+
 def search(request):
 
     if request.method == "POST":
-
+        
+        # pull user-entered data from search form
         form = newSearchForm(request.POST)
 
+        # if data is valid, pull cleaned data for user query
         if form.is_valid():
 
             q = form.cleaned_data["q"]
 
             entries = util.list_entries()
 
+            # compare user query to existing entries for case-insensitive match
+            # if match exists, redirect user directly to the entry page
             for entry in entries:
                 if entry.casefold() == q.casefold():
                     return HttpResponseRedirect(reverse("entry", kwargs={"title": entry}))
 
+            # if no case-insensitive match, filter existing entries which contain the query string
             filtered = [k for k in entries if q.casefold() in k.casefold()]
 
+            # return search results using filtered list
             return render(request, "encyclopedia/search.html", {
                 "entries": filtered, "q": q, "searchForm": form
             })
     
         else:
 
+            # if form isn't valid, return user to index
             return render(request, "encyclopedia/index.html", {
             "entries": util.list_entries(), "searchForm": form
             })
 
     else:
 
-        return render(request, "encyclopedia/search.html", {
-            "entries": [], "searchForm": newSearchForm
-        })
+        # if user GETs the search page, redirect them to the index page
+        return HttpResponseRedirect(reverse("index"))
+
 
 def new(request):
 
     if request.method == "POST":
 
+        # pull user-entered data from new entry creation form
         entryForm = newEntryForm(request.POST)
 
+        # if data passes is_valid(), check if an entry with the given title already exists
         if entryForm.is_valid():
 
             title = entryForm.cleaned_data["title"]
             entries = util.list_entries()
 
             for entry in entries:
+                # if entry with this title already exists, return with error message to user
                 if title.casefold() == entry.casefold():
                     return render(request, "encyclopedia/new.html", {
                         "searchForm": newSearchForm(), "form": entryForm, "error": f"An entry with the title '{title}' already exists."
                     })
 
-            content = newEntryForm.cleaned_data["content"]
+            # if entry doesn't already exist, grab content input
+            content = entryForm.cleaned_data["content"]
+            # save the new entry
+            util.save_entry(title, content)
+            # redirect user to the page of the entry whihc they just created and saved
+            return HttpResponseRedirect(reverse("entry", kwargs={"title": title}))
 
-            return render(request, "encyclopedia/new.html", {
-                "searchForm": newSearchForm(), "form": newEntryForm()
-            })
-
-
-
-
+    # if GET request, bring user to empty page for new entry input
     return render(request, "encyclopedia/new.html", {
         "searchForm": newSearchForm(), "form": newEntryForm()
     })
