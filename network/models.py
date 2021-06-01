0ba6b483ac826437d46a749e22e0f53669532119
Jake Bianca <jakebbianca@gmail.com>
2021-06-01 13:11:55 -0400

    added comments for potential spec of needed models

--- network/models.py
+++ network/models.py
@@ -4,3 +4,28 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+    #followers? -- needed or just count from Follows class?
+
+
+#will need model form to create new post
+#class Post(models.Model):
+    #id (auto)
+    #poster FOREIGN KEY user -- maybe call user instead?
+    #content
+    #date
+    #time
+    #datetime?
+    #likes? -- needed or will this pull from Likes class?
+
+#class Follows:
+    #id (auto)
+    #follower FOREIGN KEY user
+    #following FOREIGN KEY user
+    #active? -- instead of deleting row, just set this to 0/inactive when someone unfollows
+
+#class Likes:
+    #id (auto)
+    #user FOREIGN KEY
+    #post FOREIGN KEY
+
+
