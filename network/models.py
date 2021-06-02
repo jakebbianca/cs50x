e757bfae753910fdf416fc3433af408e1da71c13
Jake Bianca <jakebbianca@gmail.com>
2021-06-02 11:26:12 -0400

    created class for Post, Follows, Likes, then migrated

--- network/models.py
+++ network/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 
 class User(AbstractUser):
@@ -7,28 +8,18 @@ class User(AbstractUser):
     #followers? -- needed or just count from Follows class?
 
 
-#will need model form to create new post
-#class Post(models.Model):
-    #id (auto)
-    #poster FOREIGN KEY user -- maybe call user instead?
-    #content
-    #date
-    #time
-    #datetime?
-    #likes? -- needed or will this pull from Likes class?
+class Post(models.Model):
+    poster = models.ForeignKey(User, on_delete=models.CASCADE, related_name="poster")
+    content = models.CharField(max_length=280)
+    date = models.DateField()
+    time = models.TimeField()
+    datetime = models.DateTimeField()
 
-#class Follows:
-    #id (auto)
-    #follower FOREIGN KEY user
-    #following FOREIGN KEY user
-    #active? -- instead of deleting row, just set this to 0/inactive when someone unfollows
+class Follows(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name="follower")
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    active = models.BinaryField()
 
-#class Likes:
-    #id (auto)
-    #user FOREIGN KEY
-    #post FOREIGN KEY
-
-
-#LITERALLY TESTING GIT
-
-
+class Likes(models.Model):
+    liker = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liker")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="liked_post")
